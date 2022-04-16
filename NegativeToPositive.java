6d23b10a8fc5a8d8a810adde6259acd247404df0
prasanthkur <103854972+prasanthkur@users.noreply.github.com>
2022-04-16 12:52:24 -0700

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=-40;
+		if(num<0)
+		{
+			int res=(num*-1);
+			
+			System.out.print("The number is " + num);
+			System.out.println(" and converted to " +res);
+			
+		
+		}
+	}
+
+}
---
+++ PositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num =35;
+		
+		if(num<0)
+		{	
+							
+			System.out.println("The number is negative");
+		}
+			else if (num>0)
+			{
+				System.out.println("The number is positive");
+			}
+			
+			else 
+				System.out.println("The number is neither positive or negative");
+			
+
+	}
+
+}
